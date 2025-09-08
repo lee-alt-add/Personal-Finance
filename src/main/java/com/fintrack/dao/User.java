e2dd79601742606e8b59e27b66c1dfd25c693d26
lee-alt-add <lindokuhlehlatshwayo@gmail.com>
2025-09-08 16:46:54 +0200

    Completed User class

---
+++ src/main/java/com/fintrack/dao/User.java
@@ -0,0 +1,31 @@
+package com.fintrack.dao;
+
+public class User {
+	private int id;
+	private String name;
+	private String email;
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setEmail(String name) {
+		this.email = email;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+}
