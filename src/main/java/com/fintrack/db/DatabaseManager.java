54de7cca4fc84ff931acbdf234d320f4c856a2ba
lee-alt-add <lindokuhlehlatshwayo@gmail.com>
2025-09-08 17:35:13 +0200

    DatabaseManager: simplified this class. Removed start() and stop() methods and made the class static

--- src/main/java/com/fintrack/db/DatabaseManager.java
+++ src/main/java/com/fintrack/db/DatabaseManager.java
@@ -5,19 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseManager {
+	private static final String URL = "jdbc:sqlite::memory:";
 
-	private final Connection connection;
-	private final String URL = "jdbc:sqlite::memory:";
-
-	public DatabaseManager() throws SQLException {
-		this.connection = DriverManager.getConnection(URL);
-	}
-
-	public Connection getConnection() {
-		return this.connection;
-	}
-
-	public void shutdown() throws SQLException {
-		this.connection.close();
+	public static Connection getConnection() throws SQLException {
+		return DriverManager.getConnection(URL);
 	}
 }
