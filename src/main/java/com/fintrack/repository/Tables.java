9261a1af1f084e7bba6b9fbb7c019b3cc4fe657a
lee-alt-add <lindokuhlehlatshwayo@gmail.com>
2025-09-09 10:35:50 +0200

    Added Tables class for testing UserRepository class

---
+++ src/main/java/com/fintrack/repository/Tables.java
@@ -0,0 +1,37 @@
+package com.fintrack.repository;
+
+import java.sql.*;
+import java.util.*;
+
+public class Tables {
+	private Connection connection;
+
+	public Tables(Connection connection) {
+		this.connection = connection;
+	}
+
+	public boolean createTable(String sql) throws SQLException {
+		if (sql.toLowerCase().trim().contains("create table")) {
+            try (final PreparedStatement s = connection.prepareStatement(sql)) {
+                int result = s.executeUpdate();
+                return result == 0;
+            } catch (SQLException e) {
+                e.printStackTrace();
+                return false;
+            }
+        }
+        return false;
+	}
+
+	public boolean createUsers() throws SQLException {
+		String sql = """
+			CREATE TABLE users (
+				id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+				name VARCHAR(100) NOT NULL,
+				email VARCHAR(100) NOT NULL
+			);
+		""";
+
+		return createTable(sql);
+	}
+}
