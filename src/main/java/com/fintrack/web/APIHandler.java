c5e4620f77730d86ffc074dce7fb7d2c04b19793
lee-alt-add <lindokuhlehlatshwayo@gmail.com>
2025-09-09 12:58:30 +0200

    Added APIHandler class

---
+++ src/main/java/com/fintrack/web/APIHandler.java
@@ -0,0 +1,38 @@
+package com.fintrack.web;
+
+import io.javalin.http.Context;
+import java.util.Map;
+
+import com.fintrack.dao.User;
+import com.fintrack.db.DatabaseManager;
+import com.fintrack.repository.*;
+import java.sql.SQLException;
+
+public class APIHandler {
+
+	static DatabaseManager manager = new DatabaseManager();
+	static UserRepository repo = new UserRepository(manager.getConnection());
+	static Tables userTable = new Tables(manager.getConnection());
+	
+	public static void initializeDB() {
+		userTable.createUsers();
+	}
+
+	public static void getHealth(Context context) {
+		context.json(Map.of("status", "UP"));
+	}
+
+	public static void saveUser(Context context) {
+		try {
+			User user = context.bodyAsClass(User.class);
+			repo.save(user);
+			context.status(201).json(user);
+		} catch (Exception e) {
+			context.status(404);
+		}
+	}
+
+	public static void findAllUsers(Context context) {
+		context.status(201).json(repo.findAll());
+	}
+}
