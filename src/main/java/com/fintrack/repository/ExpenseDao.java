217be5836aebdaf79fdc6cf58424c5480b0e2dc5
lee-alt-add <lindokuhlehlatshwayo@gmail.com>
2025-09-12 21:13:38 +0200

    Added ExpenseDao method and it's test class

---
+++ src/main/java/com/fintrack/repository/ExpenseDao.java
@@ -0,0 +1,71 @@
+package com.fintrack.repository;
+
+
+import java.sql.*;
+import java.util.*;
+import java.time.LocalDateTime;
+import com.fintrack.dao.Expense;
+
+
+public class ExpenseDao {
+	private final Connection connection;
+
+	public ExpenseDao(Connection connection) {
+		this.connection = connection;
+	}
+
+	public Expense addExpense(Expense expense) throws SQLException {
+	    String sql = "INSERT INTO expenses (user_id, amount, category, description) VALUES (?, ?, ?, ?)";
+	    try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+	        stmt.setInt(1, expense.getUserId());
+	        stmt.setDouble(2, expense.getAmount());
+	        stmt.setString(3, expense.getCategory());
+	        stmt.setString(4, expense.getDescription());
+	        stmt.executeUpdate();
+
+	        return expense;
+	    } catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public List<Expense> findAllExpenses() {
+		List<Expense> allExpenses = new ArrayList<>();
+		String sql = "SELECT * FROM expenses;";
+
+		try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+			ResultSet results = stmt.executeQuery();
+
+			while (results.next()) {
+				allExpenses.add(new Expense (
+					results.getInt("id"),
+					results.getInt("user_id"),
+					results.getDouble("amount"),
+					results.getString("category"),
+					results.getString("description"),
+					results.getTimestamp("date").toLocalDateTime()
+				));
+			}
+			return allExpenses;
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return allExpenses;
+		}
+	}
+
+	public Expense removeExpenseById(int id) {
+		String sql = "DELETE FROM expenses WHERE expenses.id = ?";
+
+		try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+			stmt.setInt(1, id);
+			Expense expense = findAllExpenses().stream().filter(e -> e.getId() == id).toList().getFirst();
+			stmt.executeUpdate();
+			return expense;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+}
---
+++ src/test/java/com/fintrack/repository/ExpenseDaoTest.java
@@ -0,0 +1,71 @@
+package com.fintrack.repository;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.sql.*;
+import java.util.*;
+import java.time.LocalDateTime;
+
+import com.fintrack.db.DatabaseManager;
+import com.fintrack.dao.Expense;
+
+public class ExpenseDaoTest {
+	private static DatabaseManager manager;
+
+    /**
+     * Connect to the in-memory SQLite database before each test runs
+     * @throws SQLException failed to connect
+     */
+    @BeforeEach
+    public void setup() throws SQLException {
+        manager = new DatabaseManager();
+    }
+
+    /**
+     * Close the connection to the database after each tests runs
+     * @throws SQLException failed to close the connection
+     */
+    @AfterEach
+    public void cleanup() throws SQLException {
+        manager.shutdown();
+    }
+
+    @Test
+    public void saveExpenseTest() throws SQLException {
+        Tables tables = new Tables(manager.getConnection());
+        ExpenseDao expenseDao = new ExpenseDao(manager.getConnection());
+        Expense expense = new Expense(1,1, 200.00, "food", "none", LocalDateTime.now());
+
+        assertTrue(tables.createExpenses());
+        Expense expenseAdded = expenseDao.addExpense(expense);
+        assertNotNull(expenseAdded);
+        assertEquals(1,expenseAdded.getId());
+        assertNotNull(expenseDao.removeExpenseById(1));
+    }
+
+    @Test
+    public void findAllExpensesTest() throws SQLException {
+        Tables tables = new Tables(manager.getConnection());
+        ExpenseDao expenseDao = new ExpenseDao(manager.getConnection());
+        Expense expense = new Expense(1,1, 200.00, "food", "none", LocalDateTime.now());
+        Expense expensetwo = new Expense(2,1, 350.00, "gym", "none", LocalDateTime.now());
+
+        assertTrue(tables.createExpenses());
+        assertNotNull(expenseDao.addExpense(expense));
+        assertNotNull(expenseDao.addExpense(expensetwo));
+
+        List<Expense> expensesFound = expenseDao.findAllExpenses();
+        assertEquals(2, expensesFound.size());
+        System.out.println(expensesFound.getFirst().getId() + " : " + expensesFound.getLast().getId());
+        assertEquals("food", expensesFound.getFirst().getCategory());
+        assertEquals("gym", expensesFound.getLast().getCategory());
+        assertNotNull(expenseDao.removeExpenseById(1));
+        assertNotNull(expenseDao.removeExpenseById(2));
+    }
+}
